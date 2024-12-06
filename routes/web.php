5bb8c4b50ea35c073391e1f69b8aaa051040330a
Theo Foucher <theonicolas.foucher@gmail.com>
2024-12-06 10:11:57 +0100

    routes ?

--- routes/web.php
+++ routes/web.php
@@ -25,21 +25,38 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // Protection contre le Brute Force pour les autres routes sensibles
-Route::middleware(['auth', 'throttle:5,1'])->group(function () {
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Routes des paiements
-    Route::resource('/paiement', PaiementController::class);
+    Route::get('/paiement', [PaiementController::class, 'index'])->name('paiement.index');
+    Route::get('/paiement/create', [PaiementController::class, 'create'])->name('paiement.create');
+    Route::post('/paiement', [PaiementController::class, 'store'])
+        ->name('paiement.store')
+        ->middleware('throttle:2,1');
 
     // Routes des cartes de crédit
-    Route::resource('/carte-credit', CarteCreditController::class);
+    Route::get('/carte-credit', [CarteCreditController::class, 'index'])->name('carte-credit.index');
+    Route::get('/carte-credit/create', [CarteCreditController::class, 'create'])->name('carte-credit.create');
+    Route::delete('/carte-credit/destroy/{carteCredit}', [CarteCreditController::class, 'destroy'])->name('carte-credit.destroy');
+    Route::post('/carte-credit', [CarteCreditController::class, 'store'])
+        ->name('carte-credit.store')
+        ->middleware('throttle:2,1');
+
 
     // Routes des remboursements
-    Route::resource('/remboursement', RemboursementController::class);
+    Route::get('/remboursement', [RemboursementController::class, 'index'])->name('remboursement.index');
+    Route::get('/remboursement/create', [RemboursementController::class, 'create'])->name('remboursement.create');
+    Route::post('/remboursement', [RemboursementController::class, 'store'])
+        ->name('remboursement.store')
+        ->middleware('throttle:3,1');
 
+});
+
+// Routes du profil accessibles sans throttling supplémentaire
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 });
 
 require __DIR__.'/auth.php';
